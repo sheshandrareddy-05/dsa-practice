8296c12d12d0617e1270799d63e01d63a10e7464
Sheshandra Reddy.G <sheshandrareddy05@gmail.com>
2026-07-28 17:36:47 +0530

    Day 87: Solved Course Schedule II

--- README.md
+++ README.md
@@ -97,6 +97,7 @@ journey to become an ML Engineer at Google.
 | 84 | Number of Connected Components | Medium | Union Find |
 | 85 | Redundant Connection | Medium | Union Find |
 | 86 | Graph Valid Tree | Medium | Union Find / BFS |
+| 87 | Course Schedule II | Medium | Topological Sort |
 
 ## About Me
 - ECE Student @ RGM College, Nandyal
---
+++ course_schedule_2.py
@@ -0,0 +1,25 @@
+# Day 87: Course Schedule II
+# Difficulty: Medium
+# Topic: Topological Sort
+# Date: 2026-07-28
+
+from typing import List
+from collections import defaultdict, deque
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+        in_degree = [0] * numCourses
+        for a, b in prerequisites:
+            graph[b].append(a)
+            in_degree[a] += 1
+        queue = deque([i for i in range(numCourses) if in_degree[i] == 0])
+        order = []
+        while queue:
+            node = queue.popleft()
+            order.append(node)
+            for nei in graph[node]:
+                in_degree[nei] -= 1
+                if in_degree[nei] == 0:
+                    queue.append(nei)
+        return order if len(order) == numCourses else []
